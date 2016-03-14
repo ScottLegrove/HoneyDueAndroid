941613b5ab14343e451f628798e711b66e0e6709
m-windle <masatoshi.windle@gmail.com>
2016-03-14 18:06:06 -0400

    Added fields to register page

--- app/src/main/java/pragmaticdevelopment/com/honeydue/RegisterActivity.java
+++ app/src/main/java/pragmaticdevelopment/com/honeydue/RegisterActivity.java
@@ -14,6 +14,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Register");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
