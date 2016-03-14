f574174a24ebd22054e2b53fc334a1c6707c9b7d
m-windle <masatoshi.windle@gmail.com>
2016-03-14 14:08:53 -0400

    asdf

--- app/src/main/java/pragmaticdevelopment/com/honeydue/ListFragment.java
+++ app/src/main/java/pragmaticdevelopment/com/honeydue/ListFragment.java
@@ -29,6 +29,8 @@ public class ListFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    //asdfasdfasdf
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
